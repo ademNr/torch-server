df7da8c53ee0245fad75039c177c3b00b616a910
ademnr <ademnr2@gmail.com>
2025-08-21 16:30:15 +0100

    secirity handling

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ const authLimiter = rateLimit({
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+
+
         });
         console.log('✅ MongoDB connected');
 
